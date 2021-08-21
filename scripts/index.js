e0dee7ced71b5778fa2f33f4c988f51999993690
Kirill Belyshev <superboris2000@gmail.com>
2021-08-21 19:11:51 +0500

    feat: доработки по верстке и js

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,33 @@ let profile = document.querySelector('.profile');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close');
 let profileEditButton = profile.querySelector('.profile__edit-button');
+let inputName = document.querySelector('.popup__name');
+let inputSpec = document.querySelector('.popup__specialization');
+let profileName = document.querySelector('.profile__name');
+let profileDesc = document.querySelector('.profile__description');
+let form = document.querySelector('.popup__form');
+let submitButton = document.querySelector('.popup__submit');
 
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+
+function togglePopup() {
+  if(!popup.classList.contains('popup_opened')) {
+    inputName.value = profileName.textContent;
+    inputSpec.value = profileDesc.textContent;
+  }
+  popup.classList.toggle('popup_opened');
 }
-function closePopup() {
-  popup.classList.remove('popup_opened');
+
+
+profileEditButton.addEventListener('click', togglePopup);
+popupCloseButton.addEventListener('click', togglePopup);
+
+
+function formSubmitHandler (event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDesc.textContent = inputSpec.value;
+
 }
-
-
-profileEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+form.addEventListener('submit', formSubmitHandler);
+submitButton.addEventListener('click', togglePopup);
